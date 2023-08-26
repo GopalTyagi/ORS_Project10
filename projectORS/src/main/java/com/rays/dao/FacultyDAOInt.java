0c097bedfcec54568c2d7db002e0fdd314c6face
GopalTyagi <gopaltyagi124@gmail.com>
2023-08-26 07:09:22 +0530

    project

---
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * @author Gopal Tyagi 
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
