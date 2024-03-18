db2650a3180b107bedd482ba3d03727c8972c822
Gopal Tyagi <120017260+GopalTyagi@users.noreply.github.com>
2024-03-18 13:31:17 +0530

    Update TimeTableCtl.java

--- ORS_Project10/projectORS/src/main/java/com/rays/ctl/TimeTableCtl.java
+++ ORS_Project10/projectORS/src/main/java/com/rays/ctl/TimeTableCtl.java
@@ -18,7 +18,7 @@ import com.rays.service.CourseServiceInt;
 import com.rays.service.SubjectServiceInt;
 import com.rays.service.TimeTableServiceInt;
 /**
- * @author Krishnpal Verma
+ * @author Gopal Tyagi
  *
  */
 @RestController
