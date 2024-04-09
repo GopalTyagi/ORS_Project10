9e39b325e87f03c2b6f85c28e28797a916471fc2
Gopal Tyagi <120017260+GopalTyagi@users.noreply.github.com>
2024-04-09 09:22:52 +0530

    Update AttachmentServiceInt.java

--- ORS_Project10/projectORS/src/main/java/com/rays/common/attachment/AttachmentServiceInt.java
+++ ORS_Project10/projectORS/src/main/java/com/rays/common/attachment/AttachmentServiceInt.java
@@ -7,7 +7,7 @@ import com.rays.common.UserContext;
 
 /**
  * College Service interface.
- * @author Krishnpal Verma
+ * @author Gopal Tyagi
  */
 
 public interface AttachmentServiceInt extends BaseServiceInt<AttachmentDTO> {
