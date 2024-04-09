cfd67d9fc84e7d4b16b61c2bfbd81d83b994ff61
Gopal Tyagi <120017260+GopalTyagi@users.noreply.github.com>
2024-04-09 09:26:59 +0530

    Update DropdownList.java

--- ORS_Project10/projectORS/src/main/java/com/rays/common/DropdownList.java
+++ ORS_Project10/projectORS/src/main/java/com/rays/common/DropdownList.java
@@ -4,7 +4,7 @@ package com.rays.common;
  * DropdownList interface is implemented by DTOs those are used to create drop
  * down list on HTML pages
  * 
- * @author Krishnpal Verma
+ * @author Gopal Tyagi
  * 
  */
 public interface DropdownList {
