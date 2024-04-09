dcb80cac056bcb9ad953412d6cdb0348c1694416
Gopal Tyagi <120017260+GopalTyagi@users.noreply.github.com>
2024-04-09 09:32:05 +0530

    Update WebSecurityConfig.java

--- ORS_Project10/projectORS/src/main/java/com/rays/config/WebSecurityConfig.java
+++ ORS_Project10/projectORS/src/main/java/com/rays/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
- * @author Krishnpal Verma
+ * @author Gopal Tyagi
  *
  */
 @Configuration
